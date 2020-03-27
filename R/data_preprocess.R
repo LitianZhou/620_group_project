803f7d9d7198459c3015f772466d7e7c50a7a464
litian <zlt@umich.edu>
2020-03-27 02:13:09 -0400

    current flow

--- R/data_preprocess.R
+++ R/data_preprocess.R
@@ -16,13 +16,41 @@ data = list(
     daytime2 = data2df(folder = "ChenyiYu/daytime2"),
     night2 = data2df(folder = "ChenyiYu/night2")
   ),
-  
-  
+
   BangyaoZhao = data2df(folder = "BangyaoZhao"),
   
   QingzhiLiu = data2df(folder = "QingzhiLiu")
 )
 
 #check
-# data$LitianZhou %>% ggplot(mapping = aes(x = time_line)) + geom_line(mapping = aes(y =
-#                                                                         heart_rate, color = sleep))
+# data$LitianZhou %>% ggplot(mapping = aes(x = time_line)) + geom_line(mapping = aes(y = hr, color = sleep))
+
+# divide into epochs at the length of m minutes:
+m = 600
+data$LitianZhou = epoch_generation(data$LitianZhou, m)
+data$BangyaoZhao = epoch_generation(data$BangyaoZhao, m)
+data$QingzhiLiu = epoch_generation(data$QingzhiLiu, m)
+data$NingyuanWang$Day1 = epoch_generation(data$NingyuanWang$Day1, m)
+data$NingyuanWang$Day2 = epoch_generation(data$NingyuanWang$Day2, m)
+data$ChenyiYu$daytime1 = epoch_generation(data$ChenyiYu$daytime1, m)
+data$ChenyiYu$night1 = epoch_generation(data$ChenyiYu$night1, m)
+data$ChenyiYu$daytime2 = epoch_generation(data$ChenyiYu$daytime2, m)
+data$ChenyiYu$night2 = epoch_generation(data$ChenyiYu$night2, m)
+
+
+
+# use ratio to standardize the data (optional!):
+data_df = NULL
+ind = 1
+for (people in data) {
+  #optional
+  #data_df[[ind]] = mean_ratio_standardization(people) 
+  data_df[[ind]] = med_ratio_standardization(people)
+  ind = ind+1
+}
+
+# combine the data of five group members:
+total_data = bind_rows(data_df)
+save(total_data, file = "./data/total_data.csv")
+
+
