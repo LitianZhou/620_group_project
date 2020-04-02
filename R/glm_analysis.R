6e841ced4c09268382e4d442c1db923bda5f08af
bangyao zhao <byzhao@umich.edu>
2020-04-01 21:27:08 -0400

    happy

--- R/glm_analysis.R
+++ R/glm_analysis.R
@@ -1,3 +1,5 @@
+load("data/normalization1.Rdata")
+
 model = glm(
   sleep ~ mean_hr + sd_hr + mean_eda + sd_eda + mean_acc + sd_acc,
   data = total_data,
--- R/normalization.R
+++ R/normalization.R
@@ -2,7 +2,7 @@ data_epoch <- readRDS("data/data_epoch.rds")
 source('R/epoch_generation.R')
 source('R/ratio_standardization.R')
 
-############ Normalizarion 1
+############ Normalization 1
 # use ratio to standardize the data (optional!):
 data_df = NULL
 ind = 1
@@ -16,7 +16,7 @@ names(data_df) = names(data_epoch)
 
 # combine the data of five group members:
 total_data1 = bind_rows(data_df)
-# saveRDS(total_data1, file = "./data/total_data1.rds")
+# save(data_df,total_data1, file = "./data/normalization1.Rdata")
 
 ############ Normalizarion 2
 
